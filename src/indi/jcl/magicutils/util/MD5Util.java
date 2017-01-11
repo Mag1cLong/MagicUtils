6c0c55058a2f341b865ad2e6976fc27eed73b514
Mag1cLong <147263180@qq.com>
2017-01-11 17:38:53 +0800

    身份证工具类 json工具类 MD5工具类 UUID生成器

--- src/indi/jcl/magicutils/util/MD5Util.java
+++ src/indi/jcl/magicutils/util/MD5Util.java
@@ -1,7 +1,4 @@
-package indi.jcl.magicutils.util;/*
- *Copyright(C) 2012 www.eversharp.cn
- *All right reserved.
- */
+package indi.jcl.magicutils.util;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,96 +9,87 @@ import java.nio.channels.FileChannel;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-/**
- * description:
- * 
- * @author wsb
- * @version 1.0
- * @date 2011-6-27
- */
 public abstract class MD5Util {
-	/**
-	 * 默认的密码字符串组合，apache校验下载的文件的正确性用的就是默认的这个组合
-	 */
-	protected static char			hexDigits[]		= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-	protected static MessageDigest	messagedigest	= null;
-	protected static String	charset	= "UTF-8";
-	
-	static {
-		try {
-			messagedigest = MessageDigest.getInstance("MD5");
-		} catch (NoSuchAlgorithmException nsaex) {
-			System.err.println(MD5Util.class.getName() + "初始化失败，MessageDigest不支持MD5Util");
-			nsaex.printStackTrace();
-		}
-	}
+    /**
+     * 默认的密码字符串组合，apache校验下载的文件的正确性用的就是默认的这个组合
+     */
+    protected static char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    protected static MessageDigest messagedigest = null;
+    protected static String charset = "UTF-8";
 
-	/**
-	 * 适用于上G大的文件
-	 * 
-	 * @param file
-	 * @return
-	 * @throws IOException
-	 */
-	public static String getFileMD5String(File file) throws IOException {
-		FileInputStream in = new FileInputStream(file);
-		try {
-			FileChannel ch = in.getChannel();
-			try {
-				MappedByteBuffer byteBuffer = ch.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
-				messagedigest.update(byteBuffer);
-			}
-			finally {
-				ch.close();
-			}
-		}
-		finally {
-			in.close();
-		}
-		return bufferToHex(messagedigest.digest());
-	}
+    static {
+        try {
+            messagedigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException nsaex) {
+            System.err.println(MD5Util.class.getName() + "初始化失败，MessageDigest不支持MD5Util");
+            nsaex.printStackTrace();
+        }
+    }
 
-	public static String getMD5String(String s) {
-		try {
-			return getMD5String(s.getBytes(charset));
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return "";
-		}
-	}
+    /**
+     * 适用于上G大的文件
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    public static String getFileMD5String(File file) throws IOException {
+        FileInputStream in = new FileInputStream(file);
+        try {
+            FileChannel ch = in.getChannel();
+            try {
+                MappedByteBuffer byteBuffer = ch.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
+                messagedigest.update(byteBuffer);
+            } finally {
+                ch.close();
+            }
+        } finally {
+            in.close();
+        }
+        return bufferToHex(messagedigest.digest());
+    }
 
-	public static String getMD5String(byte[] bytes) {
-		messagedigest.update(bytes);
-		return bufferToHex(messagedigest.digest());
-	}
+    public static String getMD5String(String s) {
+        try {
+            return getMD5String(s.getBytes(charset));
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "";
+        }
+    }
 
-	private static String bufferToHex(byte bytes[]) {
-		return bufferToHex(bytes, 0, bytes.length);
-	}
+    public static String getMD5String(byte[] bytes) {
+        messagedigest.update(bytes);
+        return bufferToHex(messagedigest.digest());
+    }
 
-	private static String bufferToHex(byte bytes[], int m, int n) {
-		StringBuffer stringbuffer = new StringBuffer(2 * n);
-		int k = m + n;
-		for (int l = m; l < k; l++) {
-			appendHexPair(bytes[l], stringbuffer);
-		}
-		return stringbuffer.toString();
-	}
+    private static String bufferToHex(byte bytes[]) {
+        return bufferToHex(bytes, 0, bytes.length);
+    }
 
-	private static void appendHexPair(byte bt, StringBuffer stringbuffer) {
-		char c0 = hexDigits[(bt & 0xf0) >> 4];
-		char c1 = hexDigits[bt & 0xf];
-		stringbuffer.append(c0);
-		stringbuffer.append(c1);
-	}
+    private static String bufferToHex(byte bytes[], int m, int n) {
+        StringBuffer stringbuffer = new StringBuffer(2 * n);
+        int k = m + n;
+        for (int l = m; l < k; l++) {
+            appendHexPair(bytes[l], stringbuffer);
+        }
+        return stringbuffer.toString();
+    }
 
-	public static boolean checkPassword(String password, String md5PwdStr) {
-		String s = getMD5String(password);
-		return s.equals(md5PwdStr);
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(getMD5String("123456").toUpperCase());
-	}
+    private static void appendHexPair(byte bt, StringBuffer stringbuffer) {
+        char c0 = hexDigits[(bt & 0xf0) >> 4];
+        char c1 = hexDigits[bt & 0xf];
+        stringbuffer.append(c0);
+        stringbuffer.append(c1);
+    }
+
+    public static boolean checkPassword(String password, String md5PwdStr) {
+        String s = getMD5String(password);
+        return s.equals(md5PwdStr);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMD5String("123456").toUpperCase());
+    }
 }
